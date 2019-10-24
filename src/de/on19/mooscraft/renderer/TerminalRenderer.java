80769bec5cb814be58703750f8410c4f6562d7d6
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2019-10-24 23:03:12 +0200

    Improve comment for ANSI escape code usage

--- src/de/on19/mooscraft/renderer/TerminalRenderer.java
+++ src/de/on19/mooscraft/renderer/TerminalRenderer.java
@@ -1,5 +1,6 @@
 package de.on19.mooscraft.renderer;
 
+import de.on19.mooscraft.game.screens.SplashScreen;
 import org.fusesource.jansi.AnsiConsole;
 
 import static org.fusesource.jansi.Ansi.ansi;
@@ -13,6 +14,8 @@ public class TerminalRenderer implements Renderer {
     @Override
     public void printScreen(Screen s, boolean erase) {
         if (erase) {
+            // ANSI escape codes are used to control cursor location, color, and other options in the terminal
+            // by calling ansi().eraseScreen() a special escape code for clearing the screen is generated
             System.out.println(ansi().eraseScreen());
         }
         for (String line : s.getContent()) {
