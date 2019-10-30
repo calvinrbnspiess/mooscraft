1c73a2e90473f88f13022b245ea588a2dcebd95e
Alischa Thomas <alischa.thomas@gmail.com>
2019-10-30 09:27:44 +0100

    character auswahl
    - sergänzung des Charakters Hexe in der Struktur
    - auswahl der Charaktere über Scanner Type

--- src/de/on19/mooscraft/game/characters/Character.java
+++ src/de/on19/mooscraft/game/characters/Character.java
@@ -1,4 +1,22 @@
 package de.on19.mooscraft.game.characters;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Character {
-}
+    public static void main(String[] args) {
+        List<String> characters = new ArrayList<String>();
+        characters.add("Hexe Magalia");
+        characters.add("Kriegerin (Bellatrix)");
+        characters.add("weißer Magier Kelii");
+        characters.add("Schurke (Bandito)");
+        characters.add("Meerjungfraumann ( Marin)");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Wähle deinen Spielcharakter aus und tippe den Charakternamen ein");
+        String choice = scan.next();
+        System.out.println("Du betrittst Mooscraft als " + choice);
+    }
+
+
+    }
