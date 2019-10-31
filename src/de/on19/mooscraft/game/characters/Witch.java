592f3712d05c71db284f0e0237811f8ba37bb635
Alischa Thomas <alischa.thomas@gmail.com>
2019-10-31 08:55:01 +0100

    character changes

--- src/de/on19/mooscraft/game/characters/Witch.java
+++ src/de/on19/mooscraft/game/characters/Witch.java
@@ -7,7 +7,7 @@ public class Witch extends Character {
 
 
     public Witch() {
-        super(100, 10, 10, 10, 10);
+        super(25, 20, 40, 50, 30);
     }
     /**super calls constructor of Character class
      * know which constructor by comparison of used parameters
