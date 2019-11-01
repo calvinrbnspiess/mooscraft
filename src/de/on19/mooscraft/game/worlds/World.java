540dadc1ec2b34446cd7a52d6363c4f5f0f05fba
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2019-11-01 10:19:54 +0100

    changes to World

--- src/de/on19/mooscraft/game/worlds/World.java
+++ src/de/on19/mooscraft/game/worlds/World.java
@@ -13,11 +13,24 @@ public abstract class World {
     private List<Spot> spots;
     private String[] description;
     private String[] possiblePaths;
+    private final int PATH_LENGTH = 3;
 
     public List<Spot> getSpots() {
         return this.spots;
     }
 
+
+    // shuffle -> possiblePaths.push ( spots.take 1, spots.take 1, spots.take 1 ) -> push other half
+
+    public List<Spot> getFirstPath() {
+        int spotsAmount = this.getSpots().size();
+
+        return this.getSpots().subList(0, PATH_LENGTH - 1);
+    }
+
+    public List<Spot> getSecondPath() {
+        return this.getSpots().subList(PATH_LENGTH, spots.size() - 1);
+    }
 
     public World() {
         this.spots = new ArrayList<Spot>();
