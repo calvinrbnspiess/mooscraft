8f14a952519a5bb98d51f0a5491ff45c2c9e001d
larissae24 <56932706+larissae24@users.noreply.github.com>
2019-11-05 23:56:36 +0100

    welt 3 text änderung

--- src/de/on19/mooscraft/game/worlds/world3/Spot6.java
+++ src/de/on19/mooscraft/game/worlds/world3/Spot6.java
@@ -17,7 +17,7 @@ public class Spot6 extends Spot {
     @Override
     public void onEnter(Game game, Character character) {
         Screen s = new Screen();
-        s.appendLine(StringTools.centerInRow("≈≈≈ Tief im Odenwald ≈≈≈", 112));
+        s.appendLine(StringTools.centerInRow("≈≈≈ Angriff aus dem Nichts ≈≈≈", 112));
         s.append(StringTools.emptyLines(1));
         s.appendLine(game.getProgressIndicator(character));
         s.append(StringTools.emptyLines(2));
