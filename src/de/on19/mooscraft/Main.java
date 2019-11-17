ed384808ddfdf4f6fbc5363375b1eb18a025b4b9
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2019-11-17 11:41:49 +0100

    important comment

--- src/de/on19/mooscraft/Main.java
+++ src/de/on19/mooscraft/Main.java
@@ -8,6 +8,10 @@ import de.on19.mooscraft.renderer.TerminalRenderer;
 
 public class Main {
 
+    //
+    //  important comment
+    //
+
     public static void main(String[] args) {
         Renderer renderer = new TerminalRenderer();
         ActionHandler handler = new TerminalActionHandler(renderer);
